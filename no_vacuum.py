201116efe8761002abed42240213772638539a42
Clif Houck <me@clifhouck.com>
2016-11-10 16:21:19 -0600

    Add attribution to orignal source

--- no_vacuum.py
+++ no_vacuum.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+# Ripped from philipkglass on HN: https://news.ycombinator.com/item?id=12914188
+
 target = "/usr/bin/spotify"
 with open(target) as infile:
     bytes = infile.read()
